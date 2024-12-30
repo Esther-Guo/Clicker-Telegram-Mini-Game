3d75492c600e138cf9b74ac5338575368a4369f1
Esther Guo <special0831qi@gmail.com>
2024-12-30 17:52:38 +0100

    attempt to fix vercel server error

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': {
+        target: process.env.NODE_ENV === 'production' 
+          ? 'https://garf-clicker.vercel.app'
+          : 'http://localhost:3000',
+        changeOrigin: true
+      }
     }
   }
 })
